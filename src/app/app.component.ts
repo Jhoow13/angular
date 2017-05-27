0bb898ba5848a2789be4fea3e8883947f57b419e
Joel Silva <joelcsilva@outlook.com>
2017-05-27 11:50:33 -0300

    Alterando ordem do codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,24 @@
 import { Component } from '@angular/core';
 //https://angular.io/docs/ts/latest/tutorial/toh-pt3.html
+
+export class Hero{
+    id: number;
+    name: string;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'my-app',
   template: `
@@ -74,32 +93,10 @@ import { Component } from '@angular/core';
 
 export class AppComponent  { 
     title = 'Tour of Heroes';
+    heroes = HEROES
     selectedHero: Hero
+    
     onSelect(hero: Hero): void{
         this.selectedHero = hero
     }
-    /*hero : Hero = {
-        id:1,
-        name: 'Windstorm'
-    }*/
-    heroes = HEROES
 }
-
-export class Hero{
-    id: number;
-    name: string;
-}
-
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
